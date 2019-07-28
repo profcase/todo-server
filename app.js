c992192e51ed1effb0f9a0d46c9515bb0e9c46df
Denise Case <dcase@nwmissouri.edu>
2019-07-28 16:16:18 -0500

    F19

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const express = require('express')   // web framework for Node.js
 const mongoose = require('mongoose') // MongoDB object modeling  
 const morgan = require('morgan')     // HTTP request logger middleware 
 
-const DEFAULT_PORT = config.get("port");
+const DEFAULT_PORT = config.get("port")
 
 // create Express app
-const app = express();
+const app = express()
 
 // use middleware to parse requests containing json payloads
-app.use(express.json());
+app.use(express.json())
 
 // use middleware to enable cross-origin-resource-sharing for all requests
 app.use(cors())
@@ -23,7 +23,7 @@ app.use('/todo', require('./routes/todo'));
 app.use('/', (req,res)=> {res.send('Try /todo')})
 
 // Configure data access with Mongoose
-const dbURI = config.get("mongoURI");
+const dbURI = config.get("mongoURI")
 
 mongoose
   .connect(dbURI, {
@@ -31,11 +31,11 @@ mongoose
     useCreateIndex: true
   })
   .then(() => console.log('MongoDB connected.'))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err))
 
 // Use hosting values if available, otherwise default 
 const port = process.env.PORT || DEFAULT_PORT;
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'development'
 
 app.listen(port, () =>{
    // tell user where app is running
--- models/todo.js
+++ models/todo.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
 
-const TodoSchema = new Schema({
+const TodoSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    minlength: 2,
+    maxlength: 50
   },
   date: {
     type: Date,
     default: Date.now
   }
-});
+})
 
 module.exports = Todo = mongoose.model('todo', TodoSchema);
